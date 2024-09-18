58287221e0c6a1c2957457357a7c2cccbf5e2f70
Faisal Khan <faisal5189126@gmail.com>
2024-09-19 02:33:44 +0530

    Bubble Sort done.

--- BubbleSort_05.java
+++ BubbleSort_05.java
@@ -33,3 +33,26 @@ public class BubbleSort_05 {
 // Space Complexity:
 //        O(1)
 //        (In-place sorting; only a constant amount of extra space is used)
+
+
+//NOTE:
+//
+//static void swap(int a,int b){
+//    int temp=a;
+//    a=b;
+//    b=temp;
+//}
+//
+//The issue with the swap(int a, int b) method is that in Java, primitive types
+//(like int) are passed by value. This means that when you call the swap method,
+//the values of a and b are copied into the method parameters, but the original
+//variables in the calling code (such as array elements) are not modified.
+//
+//That's why use:
+//
+//static void swap(int[] arr, int i, int j){
+//    int temp = arr[i];
+//    arr[i] = arr[j];
+//    arr[j] = temp;
+//}
+
